b13cf3ae78df7630a1dd3bb6ba4dfd442f968811
Md Abdullah Al Rahib <82247365+AbdullahAlRahib@users.noreply.github.com>
2022-09-05 22:54:23 +0600

    Add files via upload

---
+++ My_Sceond_project/Login_app/models.py
@@ -0,0 +1,14 @@
+from distutils.command.upload import upload
+import profile
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    facebook_id = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to ='profile_pic', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
